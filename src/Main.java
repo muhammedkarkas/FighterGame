2a9952fc8e2950634a2da49861aff097c9e5df6a
Muhammed Karkas <muhammed.karkas@gmail.com>
2023-03-26 09:21:30 +0300

    Classes and main Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
+        Fighter marc = new Fighter("Marc" , 15 , 100, 90, 0);
+        Fighter alex = new Fighter("Alex" , 10 , 95, 100, 0);
+        Match m = new Match(marc,alex , 90 , 100);
+        m.run();
+
     }
 }
